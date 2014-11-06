2a70256ea3a916baeb1d1b6a2f4ceda5f0d9dd4a
Dave <activethrasher00@gmail.com>
2014-11-06 00:57:40 -0500

    Add a script to source mgrep and cgrep functions

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# add some useful grep functions
+
+function mgrep()
+{
+    find . -name .repo -prune -o -name .git -prune -o -path ./out -prune -o -regextype posix-egrep -iregex '(.*\/Makefile|.*\/Makefile\..*|.*\.make|.*\.mak|.*\.mk|.*\.cmake|.\/CMakeLists.txt)' -type f -print0 | xargs -0 grep --color -n "$@"
+}
+
+function cgrep()
+{
+    find . -name .repo -prune -o -name .git -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' \) -print0 | xargs -0 grep --color -n "$@"
+}
